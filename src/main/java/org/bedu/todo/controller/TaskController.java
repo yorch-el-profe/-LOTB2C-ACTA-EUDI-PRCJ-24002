757a81a0a33fc1378ed60bc0acb2c4159865c539
Jorge Ramón <jorge.ramon2128@gmail.com>
2024-12-02 20:21:12 -0600

    Enviando datos de HTML a Java

---
+++ src/main/java/org/bedu/todo/controller/TaskController.java
@@ -0,0 +1,27 @@
+package org.bedu.todo.controller;
+
+import org.bedu.todo.model.Task;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class TaskController {
+
+    /**
+     * Método que guarda la tarea que recibe
+     * del HTML en la base de datos y 
+     * redige al usuario a la página principal.
+     * 
+     * @return La redirección a la página principal.
+     */
+    @PostMapping("guardar")
+   public String saveTask(@ModelAttribute("task") Task newTask) {
+
+    System.out.println(newTask.getDescription());
+
+    // Al final redirige el navegador a la página principal (/)
+    return "redirect:/";
+   } 
+    
+}
