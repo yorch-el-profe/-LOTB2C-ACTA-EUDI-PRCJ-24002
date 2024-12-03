61329928a542da24effd094593b13748e69d812c
Jorge Ramón <jorge.ramon2128@gmail.com>
2024-12-02 21:08:38 -0600

    Creación de la tabla task

--- src/main/java/org/bedu/todo/model/Task.java
+++ src/main/java/org/bedu/todo/model/Task.java
@@ -1,7 +1,26 @@
 package org.bedu.todo.model;
 
+import org.hibernate.annotations.ColumnDefault;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    // La columna descripción es obligatoria
+    @Column(nullable = false)
     private String description;
+
+    @Column
+    @ColumnDefault("false")
     private boolean completed;
 
     public String getDescription() {
