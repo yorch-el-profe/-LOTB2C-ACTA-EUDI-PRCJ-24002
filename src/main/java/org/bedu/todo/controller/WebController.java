36904382f8cdc8ebd76ec4c10474ad87c5c3d73a
Jorge Ramón <jorge.ramon2128@gmail.com>
2024-12-02 21:30:09 -0600

    Ejemplo terminado

--- src/main/java/org/bedu/todo/controller/WebController.java
+++ src/main/java/org/bedu/todo/controller/WebController.java
@@ -1,12 +1,20 @@
 package org.bedu.todo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
 import org.bedu.todo.model.Task;
+import org.bedu.todo.repository.TaskRepository;
 
 @Controller
 public class WebController {
+
+    @Autowired
+    private TaskRepository repository;
 
     /**
      * URL: http://localhost:8080/
@@ -22,11 +30,17 @@ public class WebController {
     public String index(Model model) {
         Task empty = new Task();
 
+        // Recuperando todas las tareas de la base de datos
+        List<Task> tasks = repository.findAll();
+
         // Enviando un String llamado "author" al HTML
         model.addAttribute("author", "Bedu");
 
         // Enviando un objeto llamado "task" al HTML
         model.addAttribute("task", empty);
+
+        // Enviando una lista de tareas llamada "tasks" al HTML
+        model.addAttribute("tasks", tasks);
 
         return "index.html";
     }    
